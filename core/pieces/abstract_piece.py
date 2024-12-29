2f493384a610760026b64fa1a8af0903e4e80574
matin <matinahmadi.programmer@gmail.com>
2024-12-29 13:14:49 +0330

    create PieceDirection class
    create direction class and it methods for pieces

--- core/pieces/abstract_piece.py
+++ core/pieces/abstract_piece.py
@@ -5,11 +5,11 @@ from ..tools.pieces import PieceColor, PieceDirection, PieceId, PieceIsKilled
 
 
 class AbstractPieces(ABC):
-    def __init__(self, pos_x: int, pos_y: int, color: str) -> None:
+    def __init__(self, pos_x: int, pos_y: int, color: str, direction: str) -> None:
         self.position: Position = Position(pos_x, pos_y)
         self.color: PieceColor = PieceColor()
         self.is_killed: PieceIsKilled = PieceIsKilled()
-        self.direction: PieceDirection = PieceDirection()
+        self.direction: PieceDirection = PieceDirection(direction)
         self.picture_path: Path = Path(f"{getcwd()}\\media\\images\\pieces\\{str(self.color)}")
         self.id: PieceId = PieceId()
 
--- core/tools/pieces/direction.py
+++ core/tools/pieces/direction.py
@@ -1,2 +1,21 @@
-class PieceDirection:
+class DirectionError(Exception):
     pass
+
+
+class PieceDirection:
+    def __init__(self, direction: str):
+        self.direction: str = direction
+
+    @property
+    def direction(self) -> str:
+        return self.direction
+
+    @direction.setter
+    def direction(self, value: str) -> None:
+        lover_value = value.lower()
+        if lover_value not in ["left", "right"]:
+            raise DirectionError(f"you have set wrong direction. your direction is: {value}")
+        self.direction = lover_value
+
+    def __str__(self):
+        return self.direction
