1f23dcb01ecf78f8cd7819242c7fc5b1fec03154
matin <matinahmadi.programmer@gmail.com>
2024-12-29 13:16:29 +0330

    create PieceColor class
    create  color class and it methods for pieces

--- core/pieces/abstract_piece.py
+++ core/pieces/abstract_piece.py
@@ -7,10 +7,10 @@ from ..tools.pieces import PieceColor, PieceDirection, PieceId, PieceIsKilled
 class AbstractPieces(ABC):
     def __init__(self, pos_x: int, pos_y: int, color: str, direction: str) -> None:
         self.position: Position = Position(pos_x, pos_y)
-        self.color: PieceColor = PieceColor()
-        self.is_killed: PieceIsKilled = PieceIsKilled()
+        self.color: PieceColor = PieceColor(color)
         self.direction: PieceDirection = PieceDirection(direction)
         self.picture_path: Path = Path(f"{getcwd()}\\media\\images\\pieces\\{str(self.color)}")
+        self.is_killed: PieceIsKilled = PieceIsKilled()
         self.id: PieceId = PieceId()
 
 
--- core/tools/pieces/color.py
+++ core/tools/pieces/color.py
@@ -1,2 +1,21 @@
-class PieceColor:
+class ColorError(Exception):
     pass
+
+
+class PieceColor:
+    def __init__(self, color: str):
+        self.color: str = color
+
+    @property
+    def color(self) -> str:
+        return self.color
+
+    @color.setter
+    def color(self, value: str) -> None:
+        lover_value = value.lower()
+        if lover_value not in ["left", "right"]:
+            raise ColorError(f"you have set wrong color. your color is: {value}")
+        self.color = lover_value
+
+    def __str__(self):
+        return self.color
