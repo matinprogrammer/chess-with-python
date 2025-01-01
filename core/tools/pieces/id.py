2338ba6cff5a2fd19095230784dfc710fb1092ed
matin <matinahmadi.programmer@gmail.com>
2025-01-01 13:56:55 +0330

    create class PieceId and piece_id property

--- core/tools/pieces/id.py
+++ core/tools/pieces/id.py
@@ -1,2 +1,18 @@
-class PieceId:
+class PieceIdError(Exception):
     pass
+
+
+class PieceId:
+    id_counter = 1
+
+    def __init__(self):
+        self.piece_id = PieceId.id_counter
+        PieceId.id_counter += 1
+
+    @property
+    def piece_id(self):
+        return self.piece_id
+
+    @piece_id.setter
+    def piece_id(self, value: int):
+        raise PieceIdError("you can't assign a piece_id")
